7680694447f3aa2005b95e0f3e038892bf35fc0e
คีตศิลป์ คงสี <keetasin01596@gmail.com>
2025-04-08 17:22:59 +0700

    add ChatHistory

--- website/models.py
+++ website/models.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from flask_login import UserMixin
 from . import db
+import pytz
 
 
 class Diary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    date = db.Column(db.Date, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.now(pytz.timezone('Asia/Bangkok')))  
     level = db.Column(db.String(50), nullable=False)
     image_filename = db.Column(db.String(255), nullable=True)  
     image_caption = db.Column(db.Text, nullable=True)  
@@ -31,3 +32,11 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     diaries = db.relationship('Diary', backref='author', lazy=True)  
+
+
+class ChatHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    role = db.Column(db.String(50), nullable=False)   
+    timestamp = db.Column(db.DateTime, default=datetime.now(pytz.timezone('Asia/Bangkok'))) 
